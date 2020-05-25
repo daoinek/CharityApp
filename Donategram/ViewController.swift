8e44a56f1b2f2c58881c4cb3193b847c56becb93
Kostya Bershov <kbershov1@gmail.com>
2020-05-25 21:01:38 +0300

    Initial Commit

---
+++ Donategram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Donategram
+//
+//  Created by Kostya Bershov on 25.05.2020.
+//  Copyright © 2020 Daoinek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
